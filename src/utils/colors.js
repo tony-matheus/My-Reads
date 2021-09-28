225aa65e83bb939e5329beb75e76cee7b25782ce
Tony <mantony0148@icloud.com>
2021-09-28 20:40:33 -0300

    :art: Add ui components and colors

---
+++ src/utils/colors.js
@@ -0,0 +1,21 @@
+export const colors = {
+  red: {
+    light: '#ff6f60',
+    default: '#e53935',
+    dark: '#ab000d',
+  },
+  blue: {
+    light: '#6ab7ff',
+    default: '#1e88e5',
+    dark: '#005cb2',
+  },
+  black: {
+    default: '#333',
+  },
+  grey: {
+    light: '#a4a4a4',
+    default: '#757575',
+    dark: '#494949',
+  },
+  white: 'white',
+}
