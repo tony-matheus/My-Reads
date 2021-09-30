dc1098dd50661a23979e6ce442b3595782763cf9
Tony <mantony0148@icloud.com>
2021-09-29 23:05:06 -0300

    :art: improve Book style

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,71 @@
+import { faBook } from '@fortawesome/free-solid-svg-icons'
 import PropTypes from 'prop-types'
-import { Box, Text, Image } from '../UI'
+import { Box, Text, Image, Icon } from '../UI'
+import { colors } from '../utils/colors'
 
-export const Book = ({ title, authorName, thumbnail }) => {
+export const Book = ({ title, authorName, subtitle, thumbnail }) => {
   return (
-    <Box mr={100} position='relative'>
-      <Image src={thumbnail} width='10rem' height='15rem' mx='auto' />
-      <Text>{title}</Text>
-      <Text>{authorName}</Text>
-      <Box position='absolute' bg='red' top='10px' width='10px' height='10px'>
-        {title}
+    <>
+      <Box
+        position='relative'
+        my={30}
+        mx={10}
+        p={10}
+        maxWidth={200}
+        minWidth={200}
+        borderRadius={5}
+        bg={colors.white}
+      >
+        <Box
+          display='inline-flex'
+          position='relative'
+          overflow='hidden'
+          height='15rem'
+          mt={-30}
+          borderRadius={5}
+          boxShadow='0 16px 38px -12px rgb(0 0 0 / 56%), 0 4px 25px 0px rgb(0 0 0 / 12%), 0 8px 10px -5px rgb(0 0 0 / 20%);'
+        >
+          <Image src={thumbnail} width='10rem' height='100%' mx='auto' />
+          <Box
+            position='absolute'
+            bg='red'
+            top='10px'
+            width='10px'
+            height='10px'
+          />
+        </Box>
+        <Box>
+          <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='space-between'
+            textAlign='left'
+            height='auto'
+          >
+            <Text color={colors.yellow.dark} fontSize='1' fontWeight='bold'>
+              <Icon icon={faBook} mr={2} />
+              {title}
+              {subtitle && (
+                <Text color={colors.black.default} fontSize='14px'>
+                  {subtitle}
+                </Text>
+              )}
+            </Text>
+            <Box>Author: {authorName}</Box>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </>
   )
+}
+
+Book.defaultProps = {
+  subtitle: '',
 }
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
   authorName: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
 }
