cc32ebc3376eee7783232ac0ee606a733929c742
Tony <mantony0148@icloud.com>
2021-09-29 21:24:20 -0300

    :art:  Move UI components to outside components folder

---
+++ src/UI/index.js
@@ -0,0 +1,5 @@
+export { default as Box } from './Box'
+export { default as HR } from './HR'
+export { default as Image } from './Image'
+export { Input } from './Input'
+export { default as Text } from './Text'
